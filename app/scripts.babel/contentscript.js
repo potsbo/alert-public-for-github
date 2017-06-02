60ce6cbb5858cabaa54479f21d98cea6fa4ec5d6
Shimpei Otsubo <potsbo@gmail.com>
2017-06-02 16:37:39 +0900

    Add content scripts

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,10 @@
-'use strict';
-
-console.log('\'Allo \'Allo! Content script');
+window.addEventListener('load', () => {
+  const h1 = document.querySelector('h1.public')
+  if (h1) {
+    const publicLabel = document.createElement('span')
+    publicLabel.setAttribute('class', 'Label Label--outline v-align-middle')
+    publicLabel.setAttribute('style', 'color: red; border-color: red;')
+    publicLabel.innerHTML = 'Public'
+    h1.appendChild(publicLabel)
+  }
+})
